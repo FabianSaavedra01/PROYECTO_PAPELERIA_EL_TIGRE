e8764f2656c7b4d935f3eb558a6d5ef4211cc7ca
Fabian <fadasahe2002@gmail.com>
2022-11-07 15:24:50 -0500

    first commit

---
+++ src/VIEWS/PANELS/InitPanel.java
@@ -0,0 +1,13 @@
+package VIEWS.PANELS;
+
+import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class InitPanel extends JPanel {
+
+    public InitPanel(ActionListener actionListener){
+        this.setVisible(true);
+        this.setBackground(Color.BLACK);
+    }
+}
